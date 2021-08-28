792723b6dd67ccb3a7a6af77c3927b97cbc55334
Hussain <hussain.binginapalli@gmail.com>
2021-08-28 15:29:41 +0530

    Entity Related base (Base models & Base repositories etc..)

---
+++ AppBaseEntity/BaseRepository/BaseCommandRepository.cs
@@ -0,0 +1,34 @@
+﻿using AppBaseEntity.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBaseEntity.BaseRepository
+{
+    public class BaseCommandRepository<T> where T:DbContext
+    {
+        public IMapper _mapper { get; }
+        public T _dbContext { get; }
+
+        public BaseCommandRepository(IMapper mapper , T dbContext) 
+        {
+            _mapper = mapper;
+            _dbContext = dbContext;
+        }
+
+        public async Task<TDomainModel> Create<TDomainModel, TEntityModel>(TDomainModel domainModel) //where TEntityModel : BaseEntity 
+        {
+            var entity = _mapper.Map<TEntityModel>(domainModel);
+            _dbContext.Add(entity);
+            await _dbContext.SaveChangesAsync();
+            domainModel = _mapper.Map<TDomainModel>(entity);
+            return domainModel;
+        }
+
+        
+    }
+}
---
+++ AppBaseEntity/Models/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBaseEntity.Models
+{
+   public class BaseResponse<T>
+    {
+        public T Result { get; set; }
+        public bool Success { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
